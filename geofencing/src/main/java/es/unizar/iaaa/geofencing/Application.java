f5dd0fbbe53333cab0373a34b40434cf56acdc44
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2016-03-20 20:11:37 +0100

    Correcciones para que funcione correctamente el ejemplo de test.

--- geofencing/src/main/java/es/unizar/iaaa/geofencing/Application.java
+++ geofencing/src/main/java/es/unizar/iaaa/geofencing/Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Aplicacion {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aplicacion.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 
--- geofencing/src/test/java/es/unizar/iaaa/geofencing/web/UserControllerTest.java
+++ geofencing/src/test/java/es/unizar/iaaa/geofencing/web/UserControllerTest.java
@@ -1,6 +1,6 @@
 package es.unizar.iaaa.geofencing.web;
 
-import es.unizar.iaaa.geofencing.SwaggerConfig;
+import es.unizar.iaaa.geofencing.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,12 +13,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = SwaggerConfig.class)
+@ContextConfiguration(classes = Application.class)
 public class UserControllerTest {
 
     @Autowired
