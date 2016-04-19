1f9296cd8462d2a008837fb9c5780e979ebccc6f
Eduardo Ibáñez <eduiba94@gmail.com>
2016-04-19 20:46:12 +0200

    changes in User

--- geofencing/src/main/java/es/unizar/iaaa/geofencing/domain/User.java
+++ geofencing/src/main/java/es/unizar/iaaa/geofencing/domain/User.java
@@ -76,7 +76,7 @@ public class User {
         return password;
     }
 
-    public void setPassword(String pass) {
+    public void setPassword(String password) {
         this.password = password;
     }
 
