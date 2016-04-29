71e0bbd6953a8e758f50cf6a3f776226d4518124
Eduardo Ibáñez <eduiba94@gmail.com>
2016-04-29 22:32:20 +0200

    location app created

---
+++ geofencing-location/src/main/java/es/unizar/iaaa/geofencing/location/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package es.unizar.iaaa.geofencing.location.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
