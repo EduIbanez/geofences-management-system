748b32bdd1b57c7fe6ec1aca11c614b52c4f4395
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2016-05-02 12:11:39 +0200

    Bug: Resuelto el problema CORS

--- geofencing/src/main/java/es/unizar/iaaa/geofencing/Application.java
+++ geofencing/src/main/java/es/unizar/iaaa/geofencing/Application.java
@@ -2,6 +2,10 @@ package es.unizar.iaaa.geofencing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
@@ -11,5 +15,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
