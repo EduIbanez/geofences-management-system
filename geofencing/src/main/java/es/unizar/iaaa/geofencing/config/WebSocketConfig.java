c170bbca7b01c63e3b20757fa913f0fc889495f4
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2016-05-04 11:30:50 +0200

    Bug: Añadido lo que faltaba para que funcione todo

--- geofencing/src/main/java/es/unizar/iaaa/geofencing/config/WebSocketConfig.java
+++ geofencing/src/main/java/es/unizar/iaaa/geofencing/config/WebSocketConfig.java
@@ -2,7 +2,6 @@ package es.unizar.iaaa.geofencing.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -19,7 +18,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/locations").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/locations").setAllowedOrigins("*").withSockJS();
     }
 
 }
