cfc8f759eb25164961109a6bc0fc53cf69d86f80
Eduardo Ibáñez <eduiba94@gmail.com>
2016-05-06 15:45:43 +0200

    changes in html and adding angular to the application

---
+++ geofencing-location/src/main/resources/static/js/core.js
@@ -0,0 +1,14 @@
+var Controller = angular.module('Controller', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    $http.get('http://localhost:8080/api/notifications')
+        .success(function(data) {
+            $scope.notifications = data.message;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+}
