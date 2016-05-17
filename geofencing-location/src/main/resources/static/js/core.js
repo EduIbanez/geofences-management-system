d2d569d1e9242d417b34868c37a2167e0cb6b22d
Eduardo Ibáñez <eduiba94@gmail.com>
2016-05-17 19:35:34 +0200

    adding angular-cookies.js to the project

---
+++ geofencing-location/src/main/resources/static/js/core.js
@@ -0,0 +1,40 @@
+angular.module('core', ['ngCookies'])
+  .controller('login', function($scope, $http, $window) {
+    $scope.formData = {};
+
+    $scope.loginUser = function () {
+        $http({
+              method: 'POST',
+              url: 'http://localhost:8080/login',
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+              transformRequest: function (obj) {
+                  var str = [];
+                  for (var p in obj) {
+                      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                  }
+                  return str.join("&");
+              },
+              data: {username: $scope.formData.email, password: $scope.formData.password}
+
+            })
+            .success(function (data) {
+                $scope.formData = {};
+                $window.location.href = '/home';
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    };
+  })
+  .controller('home', function($scope, $http, $window) {
+        $scope.notifications = {};
+
+        $http.get('http://localhost:8080/api/notifications')
+            .success(function (data) {
+                $scope.notifications = data.message;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+  });
